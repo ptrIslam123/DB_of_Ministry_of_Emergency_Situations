8ef244b5c3c760a56577361412bd9e2005de1197
ptrIslam123 <asker.novikov@bk.ru>
2021-06-20 17:37:08 +0300

    make sucessfully class client and class server for swaping packages by net

--- netAPI/test_server.py
+++ netAPI/test_server.py
@@ -22,6 +22,47 @@ server.bind((
 server.listen(5)
 
 
+
+def send_package(client, package):
+        packages = split_to_list_packages(package, 5)
+
+        for pkg in packages:
+                client.send(serialization(pkg))
+                res_pkg = deserialization(client.recv(1024))
+
+                if res_pkg.get_method_type() != SUCCESSFUL_PACKAGE_RESULT:
+                        print("Eror: response.type: ", res_pkg.get_method_type())
+                        client.close()
+                        return -1
+
+                print("\t***send-successful***")
+                
+        return 0
+
+
+def recive_package(client):
+        packages = []
+
+        while True:
+
+                pkg = deserialization(client.recv(1024))
+
+                if pkg.get_method_type() == LAST_PACKAGE_TYPE:
+                        client.send(serialization(
+                                Package(SUCCESSFUL_PACKAGE_RESULT, "Ok")
+                        ))
+                        break
+
+                packages.append(pkg)
+                print("***recive-successful***")
+
+                client.send(serialization(
+                        Package(SUCCESSFUL_PACKAGE_RESULT, "Ok")
+                ))
+        
+        return join_to_one_package(packages), 0
+
+
 while True:
         try:
                 client, addr = server.accept()
@@ -34,20 +75,22 @@ while True:
                 try:
                         while True:
                                 
-                                pkg = deserialization(client.recv(1024))
-                                
-                                if pkg.get_method_type() == LAST_PACKAGE_TYPE:
-                                        print("__close connect__: ", pkg.get_data())
-                                        client.send(serialization(
-                                                Package(CLOSE_CONNECT_PAKCAGE_METHOD_TYPE, "connect destroy[server]")
-                                        ))
+                                pkg, res = recive_package(client)
+
+                                if res != 0:
                                         client.close()
+                                        print("recive__error: connection destroy__\n")
+                                        break
+
+                                print("[Client] type: ", pkg.get_method_type(), 'data: ', pkg.get_data())
+
+                                res = send_package(client, pkg)
+
+                                if res != 0:
+                                        client.close()
+                                        print("send__error: connection destroy__\n")
                                         break
                                 
-                                print("type: ", pkg.get_method_type(), 'data: ', pkg.get_data())
-                                client.send(serialization(Package(
-                                        ICMP_PACKAGE_TYPE, "Ok"
-                                )))
 
                 except KeyboardInterrupt:
                         client.close()
