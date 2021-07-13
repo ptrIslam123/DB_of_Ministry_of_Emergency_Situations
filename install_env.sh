4b43e8f1e91f6c8380ee87c02e3ae2061f1e544f
ptrIslam123 <asker.novikov@bk.ru>
2021-07-13 10:01:47 +0300

    fix script : install_env.sh: append automatic creating user.
    release version 1.3

--- install_env.sh
+++ install_env.sh
@@ -1,8 +1,21 @@
 #! bin/bash
 
+
+
+# Create directory for sqlite3 (sqlDB)
 mkdir sqlDB
+# Create file for DB
 touch sqlDB/main.db
-cd src
-python main.py
+
+
+# chenge current directopy 
+cd src/
+USER_LOGIN="root" # default user login
+USER_PASSWD="root" # default user password
+
+# Create user account
+python autointoxication.py ${USER_LOGIN} ${USER_PASSWD}
+
+
 
 echo "Installetion successful"
